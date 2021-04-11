04e56cbc3031c75d93cc9880f7c475b1ee2758b0
Marijan Brvar <marijan.brvar@gmail.com>
2021-04-12 01:13:51 +0200

    add edit and view form, replace single page form with partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    respond_to do |format|
+    if @user.update(user_params)
+      format.html {redirect_to @user, notice: "User data is updated!"}
+    else
+      format.html {render :edit, status: :unprocessable_entity }
+    end
+  end
+  end
+
   def new
     @user = User.new
   end
@@ -14,6 +38,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
